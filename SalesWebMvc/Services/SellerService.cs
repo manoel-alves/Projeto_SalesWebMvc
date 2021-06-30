5c46ce814e0517bc0d95fb351af687ddf5fe6c36
Manoel Alves <manoelann@hotmail.com>
2021-06-30 12:04:51 -0300

    Edit Seller option Added

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -1,5 +1,6 @@
 ﻿using SalesWebMvc.Data;
 using SalesWebMvc.Models;
+using SalesWebMvc.Services.Exceptions;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,6 +32,19 @@ namespace SalesWebMvc.Services {
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj) {
+            if(!_context.Seller.Any(x => x.Id == obj.Id)) {
+                throw new NotFoundException("Id Not Found!");
+            }
+            try {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e) {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
 
     }
